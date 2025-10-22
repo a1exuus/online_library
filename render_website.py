6b47038bbbe751ff20f48235aa7a78e8b657e31a
alexuus <alexexmypride@gmail.com>
2025-10-22 17:53:02 +0300

    final changes

--- render_website.py
+++ render_website.py
@@ -1,51 +1,52 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from json import loads
-
 from livereload import Server
-
 from os import makedirs
-
 from more_itertools import chunked
-
-makedirs("pages", exist_ok=True)
-
-with open('meta_data.json', 'r', encoding='utf-8') as file:
-    books_json = file.read()
-
-books = loads(books_json)
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-pages = list(chunked(books, 10))
-
-pages_count = len(pages)
-
-for i, page_books in enumerate(pages, start=1):
-    rendered_page = template.render(
-        cards=page_books,
-        page_number=i,
-        pages_count=pages_count)
-    with open(f"pages/index{i}.html", "w", encoding="utf-8") as f:
-        f.write(rendered_page)
 
 
 def on_reload():
-    rendered_page = template.render(
-        cards=pages[0],
-        page_number=1,
-        pages_count=pages_count
+    makedirs("pages", exist_ok=True)
+
+    with open('meta_data.json', 'r', encoding='utf-8') as file:
+        books_json = file.read()
+
+    books = loads(books_json)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
+
+    template = env.get_template('template.html')
+
+    books_on_page = 10
+    pages = list(chunked(books, books_on_page))
+
+    pages_count = len(pages)
+
+    for i, page_books in enumerate(pages, start=1):
+        rendered_page = template.render(
+            cards=page_books,
+            page_number=i,
+            pages_count=pages_count)
+        with open(f"pages/index{i}.html", "w", encoding="utf-8") as f:
+            f.write(rendered_page)
+        rendered_page = template.render(
+            cards=pages[0],
+            page_number=1,
+            pages_count=pages_count
+        )
     with open('index.html', 'w', encoding="utf-8") as file:
         file.write(rendered_page)
-    print('Страница обновлена!')
 
 
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.', port=8000)
+def main():
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.', port=8000)
+
+
+if __name__ == '__main__':
+    main()
